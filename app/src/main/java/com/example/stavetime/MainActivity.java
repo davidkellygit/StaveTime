f4c2185db5135a9c8feb3a55f8b681f90ba3c86d
davidkellygit <c20324901@mytudublin.ie>
2024-03-25 19:18:40 +0000

    Ability to switch between pages is now working. The 'Next' and 'Back' buttons are no longer just for aesthetics.

--- app/src/main/java/com/example/stavetime/MainActivity.java
+++ app/src/main/java/com/example/stavetime/MainActivity.java
@@ -18,6 +18,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -31,13 +34,13 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     // The attributes are defined here.
-    Button files, camera, next;
+    Button files, /*, camera, */next;
     ImageView imageView;
     Bitmap bitmap;
     Mat mat;
     int FILES_CODE = 100;
     //int CAMERA_CODE = 101;
-
+    private ActivityResultLauncher<Intent> launcher;
 
     // The onCreate function is executed as the app opens.
     // The buttons begin listening for clicks.
@@ -56,23 +59,73 @@ public class MainActivity extends AppCompatActivity {
         //camera = findViewById(R.id.camera);
         files = findViewById(R.id.files);
         imageView = findViewById(R.id.imageView);
+        next = (Button) findViewById(R.id.next);
 
-//        // Turn 'next' button invisible
-//        next.setVisibility(View.GONE);
+        // Hide the 'next' button on launch.
+//        next.setVisibility(View.INVISIBLE);
 //        // make 'files' and 'camera' button invisible
 //        files.setVisibility(View.VISIBLE);
 //        camera.setVisibility((View.VISIBLE));
 
+        // Updated way of using 'startActivityForResult'
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        try {
+                            Intent data = result.getData();
+                            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                            imageView.setImageBitmap(bitmap);
 
+                            mat = new Mat();
+                            Utils.bitmapToMat(bitmap, mat);
 
+                            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
+
+                            Utils.matToBitmap(mat, bitmap);
+                            imageView.setImageBitmap(bitmap);
+                        }
+
+                        catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+
+        // Listens for the 'files' button.
         files.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
-                startActivityForResult(intent, FILES_CODE);
+                // This is same as we go one activity to another
+                //Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+
+                // Rather than calling startActivity(intent) we use this code
+                launcher.launch(intent);
             }
         });
+
+        // Listens for the 'next' button, which switches to the next screen.
+        next.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Activity2.class);
+            startActivity(intent);
+        });
+
+//        files.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                intent.setType("image/*");
+//                startActivityForResult(intent, FILES_CODE);
+//
+//                // !! registerForActivityResult() !!
+//                // See below:
+//                // https://www.geeksforgeeks.org/how-to-use-activityforresultluncher-as-startactivityforresult-is-deprecated-in-android/
+//            }
+//        });
 
 //        camera.setOnClickListener(new View.OnClickListener(){
 //            @Override
@@ -86,53 +139,54 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Check if the 'files' button has been pressed.
-        if(requestCode==FILES_CODE && data!=null){
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
-                imageView.setImageBitmap(bitmap);
-
-                mat = new Mat();
-                Utils.bitmapToMat(bitmap, mat);
-
-                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
-
-                Utils.matToBitmap(mat, bitmap);
-                imageView.setImageBitmap(bitmap);
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-//            // The 'Next' button now appears, and the 'Files' and 'Camera' button disappear.
-//            next.setVisibility(View.VISIBLE);
-//            files.setVisibility(View.GONE);
-//            camera.setVisibility(View.GONE);
-        }
-
-//        // Check if the 'camera' button has been pressed.
-//        if(requestCode == CAMERA_CODE && data != null){
-//            bitmap = (Bitmap) data.getExtras().get("data");
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
 //
-//            mat = new Mat();
-//            Utils.bitmapToMat(bitmap, mat);
+//        // Check if the 'files' button has been pressed.
+//        if(requestCode==FILES_CODE && data!=null){
+//            try {
+//                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+//                imageView.setImageBitmap(bitmap);
 //
-//            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
+//                mat = new Mat();
+//                Utils.bitmapToMat(bitmap, mat);
 //
-//            Utils.matToBitmap(mat, bitmap);
-//            imageView.setImageBitmap(bitmap);
+//                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
 //
+//                Utils.matToBitmap(mat, bitmap);
+//                imageView.setImageBitmap(bitmap);
+//
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+////            // The 'Next' button now appears, and the 'Files' and 'Camera' button disappear.
+////            files.setVisibility(View.INVISIBLE);
+////            camera.setVisibility(View.INVISIBLE);
+////            next.setVisibility(View.VISIBLE);
+//
+//        }
+//
+////        // Check if the 'camera' button has been pressed.
+////        if(requestCode == CAMERA_CODE && data != null){
+////            bitmap = (Bitmap) data.getExtras().get("data");
+////
+////            mat = new Mat();
+////            Utils.bitmapToMat(bitmap, mat);
+////
+////            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
+////
+////            Utils.matToBitmap(mat, bitmap);
+////            imageView.setImageBitmap(bitmap);
+////
 ////            // The 'Next' button now appears, and the 'Files' and 'Camera' button disappear.
 ////            next.setVisibility(View.VISIBLE);
 ////            files.setVisibility(View.GONE);
 ////            camera.setVisibility(View.GONE);
-//        }
-
-    }
+////        }
+//
+//    }
 
 
 
