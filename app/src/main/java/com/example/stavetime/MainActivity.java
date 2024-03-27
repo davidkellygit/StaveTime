a26ab28d6f0c4e39ee4a345d7ae6a594eb26f6e5
davidkellygit <c20324901@mytudublin.ie>
2024-03-27 16:00:43 +0000

    The colours are consistent throughout the app. Consistent black background and white buttons.

--- app/src/main/java/com/example/stavetime/MainActivity.java
+++ app/src/main/java/com/example/stavetime/MainActivity.java
@@ -26,46 +26,35 @@ import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.IOException;
-
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     // The attributes are defined here.
-    Button files, /*, camera, */next;
+    Button files, next;
     ImageView imageView;
     Bitmap bitmap;
     Mat mat;
-    int FILES_CODE = 100;
-    //int CAMERA_CODE = 101;
     private ActivityResultLauncher<Intent> launcher;
 
     // The onCreate function is executed as the app opens.
-    // The buttons begin listening for clicks.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
-        if(OpenCVLoader.initDebug()) Log.d("LOADED", "success");
+        if (OpenCVLoader.initDebug()) Log.d("LOADED", "success");
         else Log.d("LOADED", "error");
 
-        getPermission();
 
-        //camera = findViewById(R.id.camera);
         files = findViewById(R.id.files);
         imageView = findViewById(R.id.imageView);
         next = (Button) findViewById(R.id.next);
 
-        // Hide the 'next' button on launch.
-//        next.setVisibility(View.INVISIBLE);
-//        // make 'files' and 'camera' button invisible
-//        files.setVisibility(View.VISIBLE);
-//        camera.setVisibility((View.VISIBLE));
 
         // Updated way of using 'startActivityForResult'
         launcher = registerForActivityResult(
@@ -84,9 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
                             Utils.matToBitmap(mat, bitmap);
                             imageView.setImageBitmap(bitmap);
-                        }
-
-                        catch (IOException e) {
+                        } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
                     }
@@ -107,6 +94,10 @@ public class MainActivity extends AppCompatActivity {
                 launcher.launch(intent);
             }
         });
+//                // !! registerForActivityResult() !!
+//                // See below:
+//                // https://www.geeksforgeeks.org/how-to-use-activityforresultluncher-as-startactivityforresult-is-deprecated-in-android/
+
 
         // Listens for the 'next' button, which switches to the next screen.
         next.setOnClickListener(v -> {
@@ -114,115 +105,8 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-//        files.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.setType("image/*");
-//                startActivityForResult(intent, FILES_CODE);
-//
-//                // !! registerForActivityResult() !!
-//                // See below:
-//                // https://www.geeksforgeeks.org/how-to-use-activityforresultluncher-as-startactivityforresult-is-deprecated-in-android/
-//            }
-//        });
-
-//        camera.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                startActivityForResult(intent, CAMERA_CODE);
-//            }
-//        });
 
     }
 
-
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Check if the 'files' button has been pressed.
-//        if(requestCode==FILES_CODE && data!=null){
-//            try {
-//                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
-//                imageView.setImageBitmap(bitmap);
-//
-//                mat = new Mat();
-//                Utils.bitmapToMat(bitmap, mat);
-//
-//                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
-//
-//                Utils.matToBitmap(mat, bitmap);
-//                imageView.setImageBitmap(bitmap);
-//
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-////            // The 'Next' button now appears, and the 'Files' and 'Camera' button disappear.
-////            files.setVisibility(View.INVISIBLE);
-////            camera.setVisibility(View.INVISIBLE);
-////            next.setVisibility(View.VISIBLE);
-//
-//        }
-//
-////        // Check if the 'camera' button has been pressed.
-////        if(requestCode == CAMERA_CODE && data != null){
-////            bitmap = (Bitmap) data.getExtras().get("data");
-////
-////            mat = new Mat();
-////            Utils.bitmapToMat(bitmap, mat);
-////
-////            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
-////
-////            Utils.matToBitmap(mat, bitmap);
-////            imageView.setImageBitmap(bitmap);
-////
-////            // The 'Next' button now appears, and the 'Files' and 'Camera' button disappear.
-////            next.setVisibility(View.VISIBLE);
-////            files.setVisibility(View.GONE);
-////            camera.setVisibility(View.GONE);
-////        }
-//
-//    }
-
-
-
-    void getPermission(){
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-//                requestPermissions(new String(){Manifest.permission.CAMERA}, 102);
-//            }
-//        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Check if the camera permission is not granted yet
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                // Request camera permission
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 102);
-            }
-        }
-    }
-
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 102 && grantResults.length > 0){
-            if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
-                getPermission();
-            }
-        }
-    }
-
-//    @SpringBootApplication
-//    public class Main {
-//        public static void main(String[] args) {
-//            SpringApplication.run(Main.class, args);
-//        }
-//    }
 
 }
